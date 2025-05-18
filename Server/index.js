b86f10f372c775601e8ab4c93e90fd006200930e
Zain Ahmad <zainahmad4708@gmail.com>
2025-05-18 12:11:47 +0500

    minor chnage

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import { adminRouter } from "./Routes/AdminRoute.js";
 
-// import { userRouter } from "./Routes/UserRoute.js";
 const app = express()
 app.use(cors({
     origin: ["http://localhost:5173"],
